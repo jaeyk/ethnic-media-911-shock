2ad5ce22b74a3318bc67554938022bdb13f3638e
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-03-09 13:22:02 -0700

    updated ITS

---
+++ functions/theme_publications.r
@@ -0,0 +1,56 @@
+
+# I adapted theme publication function from [this blog post](https://rpubs.com/Koundy/71792).
+
+theme_Publication <- function(base_size) {
+  
+  library(grid)
+  library(ggthemes)
+  
+  (theme_foundation(base_size = base_size)
+    + theme(plot.title = element_text(size = rel(1.2)),
+            text = element_text(),
+            plot.caption = element_text(hjust = 0, face= "italic"), 
+            plot.title.position = "plot", 
+            plot.caption.position =  "plot",
+            panel.background = element_rect(colour = NA),
+            plot.background = element_rect(colour = NA),
+            panel.border = element_rect(colour = NA),
+            axis.title = element_text(size = rel(1)),
+            axis.text = element_text(), 
+            axis.line = element_line(colour="black"),
+            axis.ticks = element_line(),
+            panel.grid.major = element_line(colour="#f0f0f0"),
+            panel.grid.minor = element_blank(),
+            legend.key = element_rect(colour = NA),
+            legend.position = "bottom",
+            legend.direction = "horizontal",
+            legend.title = element_text(),
+            plot.margin=unit(c(10,5,5,5),"mm"),
+            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+            strip.text = element_text()
+    ))
+  
+}
+
+scale_fill_Publication <- function(...){
+  library(scales)
+  discrete_scale("fill","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+
+scale_colour_Publication <- function(...){
+  library(scales)
+  discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+
+# The following code comes from [*-this blog post
+
+ditch_the_axes <- theme(
+  axis.text = element_blank(),
+  axis.line = element_blank(),
+  axis.ticks = element_blank(),
+  panel.border = element_blank(),
+  panel.grid = element_blank(),
+  axis.title = element_blank()
+)
