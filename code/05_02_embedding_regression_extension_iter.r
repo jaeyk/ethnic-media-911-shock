382c3abf0bbca7e412e5881db09b454a6686bd6b
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-10-21 16:44:12 -0700

    overall change

--- code/05_02_embedding_regression_extension_iter.r
+++ code/05_02_embedding_regression_extension_iter.r
@@ -56,31 +56,35 @@ get_mean_mod <- function(corpus_copy) {
     filter(group %in% c("Arab", "Indian")) %>%
     mutate(group = if_else(str_detect(group, "Arab"), 1, 0))
 
+  quant_corpus <- quanteda::corpus(corpus, text_field = "clean_text")
+  toks <- quanteda::tokens(quant_corpus)
+  
     ## ----------------------------------------
   load(file = here("processed_data/context_bg_ai.Rdata"))
        
   mod1 <- conText(formula = terror ~ intervention + group, 
-            data = corpus, 
-            text_var = 'clean_text', 
-            pre_trained = local_glove,
-            transform = TRUE, 
-            transform_matrix = local_transform, 
-            bootstrap = TRUE, 
-            num_bootstraps = 200, 
-            stratify_by = c('intervention', 'group'),
-            permute = TRUE, 
-            num_permutations = 200, 
-            window = 6, 
-            valuetype = 'fixed', 
-            case_insensitive = TRUE, 
-            hard_cut = FALSE,
-            verbose = FALSE)
+                  data = toks, 
+                  pre_trained = local_glove,
+                  transform = TRUE, 
+                  transform_matrix = local_transform, 
+                  bootstrap = TRUE, 
+                  num_bootstraps = 20, 
+                  stratify = TRUE,
+                  permute = TRUE, 
+                  num_permutations = 200, 
+                  window = 6, 
+                  valuetype = 'fixed', 
+                  case_insensitive = TRUE, 
+                  hard_cut = FALSE,
+                  verbose = FALSE)
 
   ## ----------------------------------------
   corpus <- corpus_copy %>%
     filter(group %in% c("Arab", "Filipino")) %>%
     mutate(group = if_else(str_detect(group, "Arab"), 1, 0))
   
+  quant_corpus <- quanteda::corpus(corpus, text_field = "clean_text")
+  toks <- quanteda::tokens(quant_corpus)
   
   ## ----eval = FALSE------------------------
   ## set.seed(20201008L)
@@ -94,22 +98,24 @@ get_mean_mod <- function(corpus_copy) {
   ## ----------------------------------------
   load(file = here("processed_data/context_bg_af.Rdata"))
   
+  quant_corpus <- quanteda::corpus(corpus, text_field = "clean_text")
+  toks <- quanteda::tokens(quant_corpus)
+  
   mod2 <- conText(formula = terror ~ intervention + group, 
-            data = corpus, 
-            text_var = 'clean_text', 
-            pre_trained = local_glove,
-            transform = TRUE, 
-            transform_matrix = local_transform, 
-            bootstrap = TRUE, 
-            num_bootstraps = 200, 
-            stratify_by = c('intervention', 'group'),
-            permute = TRUE, 
-            num_permutations = 200, 
-            window = 6, 
-            valuetype = 'fixed', 
-            case_insensitive = TRUE, 
-            hard_cut = FALSE, 
-            verbose = FALSE)
+                  data = toks, 
+                  pre_trained = local_glove,
+                  transform = TRUE, 
+                  transform_matrix = local_transform, 
+                  bootstrap = TRUE, 
+                  num_bootstraps = 20, 
+                  stratify = TRUE,
+                  permute = TRUE, 
+                  num_permutations = 200, 
+                  window = 6, 
+                  valuetype = 'fixed', 
+                  case_insensitive = TRUE, 
+                  hard_cut = FALSE,
+                  verbose = FALSE)
   
   ## ----------------------------------------
   corpus <- corpus_copy %>%
@@ -120,25 +126,24 @@ get_mean_mod <- function(corpus_copy) {
   load(here("processed_data/context_bg_aa.Rdata"))
   
   mod3 <- conText(formula = terror ~ intervention + group, 
-            data = corpus, 
-            text_var = 'clean_text', 
-            pre_trained = local_glove,
-            transform = TRUE, 
-            transform_matrix = local_transform, 
-            bootstrap = TRUE, 
-            num_bootstraps = 200, 
-            stratify_by = c('intervention', 'group'),
-            permute = TRUE, 
-            num_permutations = 200, 
-            window = 6, 
-            valuetype = 'fixed', 
-            case_insensitive = TRUE, 
-            hard_cut = FALSE, 
-            verbose = FALSE)
+                  data = toks, 
+                  pre_trained = local_glove,
+                  transform = TRUE, 
+                  transform_matrix = local_transform, 
+                  bootstrap = TRUE, 
+                  num_bootstraps = 20, 
+                  stratify = TRUE,
+                  permute = TRUE, 
+                  num_permutations = 200, 
+                  window = 6, 
+                  valuetype = 'fixed', 
+                  case_insensitive = TRUE, 
+                  hard_cut = FALSE,
+                  verbose = FALSE)
   
-  out <- mean(mod1$normed_betas$Normed_Estimate[1]/mod1$normed_betas$Normed_Estimate[2],
-  mod2$normed_betas$Normed_Estimate[1]/mod2$normed_betas$Normed_Estimate[2],
-  mod3$normed_betas$Normed_Estimate[1]/mod3$normed_betas$Normed_Estimate[2])
+  out <- mean(mod1@normed_cofficients$Normed_Estimate[1]/mod1@normed_cofficients$Normed_Estimate[2],
+              mod2@normed_cofficients$Normed_Estimate[1]/mod2@normed_cofficients$Normed_Estimate[2],
+              mod3@normed_cofficients$Normed_Estimate[1]/mod3@normed_cofficients$Normed_Estimate[2])
   
   return(out)
 }
